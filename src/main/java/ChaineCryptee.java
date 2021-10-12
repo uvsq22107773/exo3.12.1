220bd32812a36488e7df582f7bf6e6165a73da5b
uvsq22107773 <abd-elmoumen.drici@ens.uvsq.fr>
2021-10-12 19:00:42 +0200

    final

--- src/main/java/ChaineCryptee.java
+++ src/main/java/ChaineCryptee.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
 public class ChaineCryptee {
-    private String SS;
+    private String SS,C;
     private int CLE;
 
     ChaineCryptee(String s, int cle){
         this.SS=s;
         this.CLE=cle;
 
-        if (this.SS==""){
+        if (this.SS==null){
             System.out.println("la chaine est NULL");
+            this.SS="";
         }
+
         if (this.CLE==0){
             System.out.println("cle egale a zero ");
         }
@@ -18,19 +20,29 @@ public class ChaineCryptee {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter Chaine : ");
+
         ChaineCryptee chaine = new ChaineCryptee(myObj.nextLine(),2);
 
         System.out.println("String : "+chaine.SS);
         String cipher = chaine.crypte();
         System.out.println("Crypted cle "+chaine.CLE+" : "+cipher);
-        System.out.println("DeCrypted cle "+chaine.CLE+" : "+chaine.decrypte(cipher));
+        System.out.println("DeCrypted cle "+chaine.CLE+" : "+chaine.decrypte());
+
+        System.out.println("\n==================\n");
+        chaine = deCryptee("ABC",2);
+        System.out.println("String : "+chaine.SS);
+
+        String c = chaine.crypte();
+        chaine = deEnClair(c,2);
+        System.out.println("Crypted : "+c);
+
 
     }
-    String decrypte(String s){
+    String decrypte(){
         int cle = -this.CLE;
         String newstring = "";
-        for (int i=0 ; i<s.length();i++){
-            newstring = newstring + decaleCaractere(s.charAt(i),cle);
+        for (int i=0 ; i<this.C.length();i++){
+            newstring = newstring + decaleCaractere(this.C.charAt(i),cle);
         }
         return newstring;
     }
@@ -39,10 +51,18 @@ public class ChaineCryptee {
         for (int i=0 ; i<this.SS.length();i++){
             newstring = newstring + decaleCaractere(this.SS.charAt(i),this.CLE);
         }
+        this.C = newstring;
         return newstring;
     }
     private static char decaleCaractere(char c, int decalage) {
         return (c < 'A' || c > 'Z')? c : (char)(((c - 'A' + decalage) % 26) + 'A');
+    }
+
+    public static ChaineCryptee deCryptee(String crypted, int K){
+        return new ChaineCryptee(crypted,K);
+    }
+    public static ChaineCryptee deEnClair(String decrypted, int K){
+        return new ChaineCryptee(decrypted,K);
     }
 
 }
